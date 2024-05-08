eabc0681818c7db3564d8846aee37939de07a610
Lemos0250 <vitorlemos703@gmail.com>
2024-05-08 20:48:30 -0300

    Shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
+
+export function App() {
+  return (
+    <div className='p-6 max-w-4xl mx-auto'>
+      <div className='border rounded-lg p-2'>
+        <Table>
+          <TableHeader>
+            <TableHead>ID</TableHead>
+            <TableHead>Produto</TableHead>
+            <TableHead>Preço</TableHead>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10}).map((_,i) => {
+              return (
+              <TableRow key={i}>
+                <TableCell>289fh9123hf103frh9</TableCell>
+                <TableCell>Produto 01 {i}</TableCell>
+                <TableCell>R$ 192,00</TableCell>
+              </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
+
+export default App
